4b617c9c85fc05c6513901618e1461f687686dcc
awangga01 <alfianawangga15@gmail.com>
2023-12-06 18:36:55 +0900

    tab layout

---
+++ app/src/main/java/com/example/task_androidadvance/adapters/ViewPagerAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.task_androidadvance.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.task_androidadvance.fragments.AlarmFragment
+import com.example.task_androidadvance.fragments.BrainTrainerFragment
+
+class ViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                AlarmFragment()
+            }
+            else -> {
+                BrainTrainerFragment()
+            }
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position) {
+            0 -> "Alarm Manager"
+            else -> {
+                return "Brain Trainer"
+            }
+        }
+    }
+}
