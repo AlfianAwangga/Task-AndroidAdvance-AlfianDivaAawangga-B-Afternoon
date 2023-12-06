7a6ff9af7964016a94111a84c5901a9b442fab7f
awangga01 <alfianawangga15@gmail.com>
2023-12-06 17:41:01 +0900

    animation - splash screen

---
+++ app/src/main/java/com/example/task_androidadvance/activities/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.task_androidadvance.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.task_androidadvance.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1900)
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
